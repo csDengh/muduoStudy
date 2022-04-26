39d7b0533283521d322186d0db4a6844b4937601
frank <csdengh@outlook.com>
2022-04-26 22:48:29 +0800

    add test

---
+++ build/src/CMakeFiles/muduo_study.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/acceptor.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/acceptor.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/buffer.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/buffer.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/channel.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/channel.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/currentThread.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/currentThread.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/defaultPoller.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/defaultPoller.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/epoller.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/epoller.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/eventLoop.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/eventLoop.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/eventLoopThread.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/eventLoopThread.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/eventLoopThreadPool.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/eventLoopThreadPool.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/inetAddress.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/inetAddress.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/logger.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/logger.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/poller.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/poller.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/socket.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/socket.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/tcpConnection.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/tcpConnection.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/tcpServer.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/tcpServer.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/thread.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/thread.cc.o"
+  "/root/cpp_project/muduoStudy/muduo_from_scratch/src/timestamp.cc" "/root/cpp_project/muduoStudy/muduo_from_scratch/build/src/CMakeFiles/muduo_study.dir/timestamp.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/muduo_study.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/muduo_study.dir/acceptor.cc.o"
+  "CMakeFiles/muduo_study.dir/buffer.cc.o"
+  "CMakeFiles/muduo_study.dir/channel.cc.o"
+  "CMakeFiles/muduo_study.dir/currentThread.cc.o"
+  "CMakeFiles/muduo_study.dir/defaultPoller.cc.o"
+  "CMakeFiles/muduo_study.dir/epoller.cc.o"
+  "CMakeFiles/muduo_study.dir/eventLoop.cc.o"
+  "CMakeFiles/muduo_study.dir/eventLoopThread.cc.o"
+  "CMakeFiles/muduo_study.dir/eventLoopThreadPool.cc.o"
+  "CMakeFiles/muduo_study.dir/inetAddress.cc.o"
+  "CMakeFiles/muduo_study.dir/logger.cc.o"
+  "CMakeFiles/muduo_study.dir/poller.cc.o"
+  "CMakeFiles/muduo_study.dir/socket.cc.o"
+  "CMakeFiles/muduo_study.dir/tcpConnection.cc.o"
+  "CMakeFiles/muduo_study.dir/tcpServer.cc.o"
+  "CMakeFiles/muduo_study.dir/thread.cc.o"
+  "CMakeFiles/muduo_study.dir/timestamp.cc.o"
+  "../../lib/libmuduo_study.pdb"
+  "../../lib/libmuduo_study.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/muduo_study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
